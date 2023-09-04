31d22032d543b2b3f091e72943b495f2a10b4321
Oleg Barovskiy <oleg.sour@gmail.com>
2023-09-04 16:23:13 +0300

    fixtures

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
-# Create your views here.
+
 def index(request):
     context = {
         'title': 'Test Title',
@@ -10,19 +11,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': 23725,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
